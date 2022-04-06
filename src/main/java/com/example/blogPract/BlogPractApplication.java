fbf8a0101ef6516c832a798136ee5d10b71116fd
shankar87dayal <shankar.87.dayal@gmail.com>
2022-04-06 23:07:00 +0530

    First commit

---
+++ src/main/java/com/example/blogPract/BlogPractApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogPract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogPract/BlogPractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogPract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
